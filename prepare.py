dc8be0867543d5826834e40304a732ec40d5dc18
Mark Foster <markdouglasfoster@gmail.com>
2022-10-14 11:42:47 -0500

    Add prep_titanic function

--- prepare.py
+++ prepare.py
@@ -12,3 +12,12 @@ def prep_iris():
     df = pd.concat([df, dummy_df], axis=1)
     
     return df
+
+def prep_titanic():
+    df = acquire.get_titanic_data()
+    df.drop(columns=['embarked', 'class', 'deck', 'age'], inplace=True)
+    df['embark_town'] = df.embark_town.fillna(value='Southampton')
+    dummy_df = pd.get_dummies(df[['sex','embark_town']], dummy_na=False, drop_first=[True, True])
+    df = pd.concat([df, dummy_df], axis=1)
+    
+    return df
