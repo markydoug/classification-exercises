5f7cd49345a402c750641f6065dd2ead0a4edae7
Mark Foster <markdouglasfoster@gmail.com>
2022-10-23 19:43:52 -0500

    Add docstring to functions

--- acquire.py
+++ acquire.py
@@ -6,6 +6,11 @@ from env import user, password, host
 from pydataset import data
 
 def get_db_url(database):
+    '''
+    Takes in the name of a database and returns the 
+    url to access that database.
+    '''
+
     return f'mysql+pymysql://{user}:{password}@{host}/{database}'
 
 def new_titanic_data():
@@ -20,10 +25,18 @@ def new_titanic_data():
 
 #function to go get passenger data from the titanic database
 def get_titanic_data():
+    '''
+    Checks to see if there is titanic data locally stored
+    if none is there it will go get the titanic data, return it
+    as a dataframe and then save it as csv file.
+    '''
+    
     filename = 'titanic.csv'
+    
     #if cached file exists use it
     if os.path.isfile(filename):
         df = pd.read_csv(filename)
+   
     #else go get the data
     else:
         df = new_titanic_data()
@@ -34,10 +47,16 @@ def get_titanic_data():
 
 #function to go get species data from the iris database
 def get_iris_data():
+    '''Checks to see if there is titanic data locally stored
+    if none is there it will go get the iris data, return it
+    as a dataframe and then save it as csv file.'''
+    
     filename = 'iris.csv'
+   
     #if cached file exists use it
     if os.path.isfile(filename):
         df = pd.read_csv(filename)
+   
     #else go get the data
     else:
         df =  pd.read_sql('''SELECT * FROM species 
@@ -50,10 +69,16 @@ def get_iris_data():
 
 #function to go get customer data from the telco_churn database
 def get_telco_data():
+    '''Checks to see if there is titanic data locally stored
+    if none is there it will go get the telco data, return it
+    as a dataframe and then save it as csv file.'''
+    
     filename = 'telco.csv'
+   
     #if cached file exists use it
     if os.path.isfile(filename):
         df = pd.read_csv(filename)
+   
     #else go get the data
     else:
         df = pd.read_sql('''SELECT * 
